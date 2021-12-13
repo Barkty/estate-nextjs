8907d72c7116d92d93e5adb4c67463fbe33a957e
Kotoye Gbemisola <58271499+Barkty@users.noreply.github.com>
2021-12-13 17:09:39 +0100

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,10 +2,10 @@ import Link from 'next/link';
 import { Menu, MenuButton, MenuList, MenuItem, IconButton, Flex, Box, Spacer } from '@chakra-ui/react';
 import { FcMenu, FcHome, FcAbout } from 'react-icons/fc';
 import { BsSearch } from 'react-icons/bs';
-import Fikey from 'react-icons/fi';
+import { Fikey } from 'react-icons/fi';
 
 const Navbar = () => (
-    <Flex p="2" borderBottom="1px" borderColor="gray.100">
+    <Flex p="2" borderBottom="1px" borderColo="gray.100">
         <Box fontSize="3x1" color="blue.400" fontWeight="bold">
             <Link href="/" paddingLeft="2">Realtor</Link>
         </Box>
