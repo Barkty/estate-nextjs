511f161b2e4e2edf317d0fa78d81c48b7ac5d5ce
Kotoye Gbemisola <58271499+Barkty@users.noreply.github.com>
2022-01-23 14:23:42 +0100

    fix up

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,15 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const baseURL = 'https://bayut.p.rapidapi.com';
-
+export const baseUrl = 'https://bayut.p.rapidapi.com';
 
 export const fetchApi = async (url) => {
-    const { data }  = await axios.get((url), {
-        headers: {
-            'x-rapidapi-host': 'bayut.p.rapidapi.com',
-            'x-rapidapi-key': '7fa8fe36camshc5460c9bfb45c69p120a25jsnb962f8e4ad4a'
-        }
-    })
-
-    return data;
+  const { data } = await axios.get((url), {
+    headers: {
+      'x-rapidapi-host': 'bayut.p.rapidapi.com',
+      'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY ,
+    },
+  });
+    
+  return data;
 }
